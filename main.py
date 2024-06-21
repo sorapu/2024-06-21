f09612b531a67d509cda49befd468f7c75fd45e6
sorapu <166776903+sorapu@users.noreply.github.com>
2024-06-21 10:50:27 +0900

    now2

--- main.py
+++ main.py
@@ -12,25 +12,3 @@ async def root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
-
-    
-{
-  "name": "express-hello-world",
-  "version": "1.0.0",
-  "description": "Express Hello World on Render",
-  "main": "app.js",
-  "repository": "https://github.com/render-examples/express-hello-world",
-  "author": "Render Developers",
-  "license": "MIT",
-  "private": false,
-  "scripts": {
-    "start": "node app.js"
-  },
-  "dependencies": {
-    "express": "^4.18.2",
-    "express-ws": "^5.0.2"
-  },
-  "engines": {
-    "node": ">=14"
-  }
-}
